a7263006b3dbd3f640c6d51f7afc2faba8a43c60
Dmytro Borisov <d.borisov@levi9.com>
2015-12-18 15:48:16 +0200

    Fixing backend auth issue

--- backend/api/policies/jwtAuth.js
+++ backend/api/policies/jwtAuth.js
@@ -1,31 +1,36 @@
 'use strict';
 
 const jwt = require('jwt-simple'),
-      moment = require('moment');
+const moment = require('moment');
+
+function returnAuthFailed(res) {
+    return res.status(401).send({
+        message: 'Authentication failed'
+    });
+}
 
 module.exports = function (req, res, next) {
     if (!req.headers || !req.headers.authorization) {
-        return res.status(401).send({
-            message: 'Authentication failed'
-        });
+        return returnAuthFailed(res);
     }
 
-    const authorizationSplitted = req.headers.authorization.split(' '),
-          tokenType = authorizationSplitted[0],
-          token = authorizationSplitted[1];
+    const authorizationSplitted = req.headers.authorization.split(' ');
+    const tokenType = authorizationSplitted[0];
+    const token = authorizationSplitted[1];
 
     if (tokenType !== 'Bearer') {
-        return res.status(401).send({
-            message: 'Authentication failed'
-        })
+        return returnAuthFailed(res)
     }
 
-    const payload = jwt.decode(token, sails.config.auth.local.secret, sails.config.auth.local.jwtSigningAlgorithm);
-
+    let payload;
+    try {
+        payload = jwt.decode(token, sails.config.auth.local.secret, false, sails.config.auth.local.jwtSigningAlgorithm);
+    }
+    catch (e) {
+        return returnAuthFailed(res)
+    }
     if (!payload.sub || payload.exp < moment().unix()) {
-        return res.status(401).send({
-            message: 'Authentication failed'
-        });
+        return returnAuthFailed(res)
     }
 
     next();
